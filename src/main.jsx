aec23271e97351a42088eb7afa93c9469991aa56
krimo30101975 <selematous@gmail.com>
2025-04-08 07:03:59 +0200

    premier commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
+import App from './App'
+
+const root = document.getElementById("root");
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+     <Routes>
+       <Route index element={<App />} />
+     </Routes>
+   </BrowserRouter>
   </StrictMode>,
 )
+
+//ReactDOM.createRoot(root).render(
+//    <BrowserRouter>
+//      <Routes>
+//        <Route index element={<App />} />
+//      </Routes>
+//    </BrowserRouter>
+//);
